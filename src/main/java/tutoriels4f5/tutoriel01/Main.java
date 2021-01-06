3483b4f877f99095f060f4f236818d95efe78c86
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-01-06 10:34:28 -0500

    test dépendance à ntro4f5

---
+++ src/main/java/tutoriels4f5/tutoriel01/Main.java
@@ -0,0 +1,14 @@
+package tutoriels4f5.tutoriel01;
+
+import ntro4f5.debogage.J;
+
+public class Main {
+	
+	public static void main(String[] args){
+		J.appel(Main.class);
+		
+		J.valeurs("ICI!");
+		
+	}
+
+}
