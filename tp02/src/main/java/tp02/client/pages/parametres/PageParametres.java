3afe5ce303e06e562b70f50c47588547faedcb7c
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-01-07 09:00:22 -0500

    en cours

--- tp02/src/main/java/tp02/client/pages/parametres/PageParametres.java
+++ tp02/src/main/java/tp02/client/pages/parametres/PageParametres.java
@@ -8,9 +8,10 @@ import ntro.debogage.DoitEtre;
 import ntro.debogage.J;
 import ntro.javafx.ChargeurDeVue;
 import ntro.javafx.Initialisateur;
-import tp02.client.pages.accueil.controleurs.ControleurAccueilFX;
-import tp02.client.pages.accueil.vues.VueAccueilFX;
+import tp02.client.pages.parametres.afficheurs.AfficheurParametres;
+import tp02.client.pages.parametres.afficheurs.AfficheurParametresFX;
 import tp02.client.pages.parametres.controleurs.ControleurParametresFX;
+import tp02.client.pages.parametres.modeles.Parametres;
 import tp02.client.pages.parametres.vues.VueParametresFX;
 
 import static tp02.client.Constantes.*;
@@ -39,9 +40,13 @@ public class PageParametres extends Application {
 
 		VueParametresFX vue = chargeur.getVue();
 		
+		Parametres parametres = new Parametres();
+		
+		AfficheurParametres<VueParametresFX> afficheurParametres = new AfficheurParametresFX();
+		
 		DoitEtre.nonNul(vue);
 
-		FabriqueControleur.creerControleur(ControleurParametresFX.class, vue);
+		FabriqueControleur.creerControleur(ControleurParametresFX.class, parametres, vue, afficheurParametres);
 
 		Scene scene = chargeur.nouvelleScene(LARGEUR_PIXELS, HAUTEUR_PIXELS);
 
@@ -49,9 +54,6 @@ public class PageParametres extends Application {
 		
 		fenetrePrincipale.setMinWidth(LARGEUR_PIXELS);
 		fenetrePrincipale.setMinHeight(HAUTEUR_PIXELS);
-
-		fenetrePrincipale.setMaxWidth(LARGEUR_PIXELS);
-		fenetrePrincipale.setMaxHeight(HAUTEUR_PIXELS);
 
 		fenetrePrincipale.show();
 	}
