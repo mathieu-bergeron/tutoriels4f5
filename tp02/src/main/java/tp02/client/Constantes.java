293300ed3885f200265f9e6847535a4a99df1071
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-01-07 06:51:06 -0500

    en cours

--- tp02/src/main/java/tp02/client/Constantes.java
+++ tp02/src/main/java/tp02/client/Constantes.java
@@ -13,11 +13,11 @@ public class Constantes {
 	public static final String CHEMIN_PARAMETRES_FXML = "/parametres/structure.xml";
 	public static final String CHEMIN_PARAMETRES_CSS = "/parametres/style.css";
 
-	public static final String CHEMIN_PARTIE_LOCALE_FXML = "/partieLocale/structure.xml";
-	public static final String CHEMIN_PARTIE_LOCALE_CSS = "/partieLocale/style.css";
+	public static final String CHEMIN_PARTIE_LOCALE_FXML = "/partie/locale/structure.xml";
+	public static final String CHEMIN_PARTIE_LOCALE_CSS = "/partie/locale/style.css";
 
-	public static final String CHEMIN_PARTIE_RESEAU_FXML = "/partieReseau/structure.xml";
-	public static final String CHEMIN_PARTIE_RESEAU_CSS = "/partieReseau/style.css";
+	public static final String CHEMIN_PARTIE_RESEAU_FXML = "/partie/reseau/structure.xml";
+	public static final String CHEMIN_PARTIE_RESEAU_CSS = "/partie/reseau/style.css";
 
 	public static final int LARGEUR_PIXELS = 340;
 	public static final int HAUTEUR_PIXELS = 500;
