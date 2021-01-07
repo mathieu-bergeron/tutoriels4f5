501bbae035b83a0799e950f343547291baab0baf
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-01-07 06:38:47 -0500

    en cours

--- tp02/src/main/java/tp02/client/Constantes.java
+++ tp02/src/main/java/tp02/client/Constantes.java
@@ -7,17 +7,17 @@ public class Constantes {
 	
 	public static final String CHEMIN_CHAINES = "traductions.chaines";
 
-	public static final String CHEMIN_PRINCIPAL_FXML = "/fxml/accueil.xml";
-	public static final String CHEMIN_PRINCIPAL_CSS = "/css/accueil.css";
+	public static final String CHEMIN_PRINCIPAL_FXML = "/accueil/structure.xml";
+	public static final String CHEMIN_PRINCIPAL_CSS = "/accueil/style.css";
 
-	public static final String CHEMIN_PARAMETRES_FXML = "/fxml/parametres.xml";
-	public static final String CHEMIN_PARAMETRES_CSS = "/css/parametres.css";
+	public static final String CHEMIN_PARAMETRES_FXML = "/parametres/structure.xml";
+	public static final String CHEMIN_PARAMETRES_CSS = "/parametres/style.css";
 
-	public static final String CHEMIN_PARTIE_LOCALE_FXML = "/fxml/partieLocale.xml";
-	public static final String CHEMIN_PARTIE_LOCALE_CSS = "/css/partieLocale.css";
+	public static final String CHEMIN_PARTIE_LOCALE_FXML = "/partieLocale/structure.xml";
+	public static final String CHEMIN_PARTIE_LOCALE_CSS = "/partieLocale/style.css";
 
-	public static final String CHEMIN_PARTIE_RESEAU_FXML = "/fxml/partieReseau.xml";
-	public static final String CHEMIN_PARTIE_RESEAU_CSS = "/css/partieReseau.css";
+	public static final String CHEMIN_PARTIE_RESEAU_FXML = "/partieReseau/structure.xml";
+	public static final String CHEMIN_PARTIE_RESEAU_CSS = "/partieReseau/style.css";
 
 	public static final int LARGEUR_PIXELS = 340;
 	public static final int HAUTEUR_PIXELS = 500;
