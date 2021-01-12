5207a973ba3a4097dd57da8bc2193708b45eb4d4
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-01-12 16:22:52 -0500

    transmettre paramètres

--- demo/src/main/java/demo/client/pages/parametres/controleurs/ControleurParametres.java
+++ demo/src/main/java/demo/client/pages/parametres/controleurs/ControleurParametres.java
@@ -2,8 +2,11 @@ package demo.client.pages.parametres.controleurs;
 
 import ntro.client.mvc.controleurs.ControleurModeleVue;
 import ntro.client.mvc.controleurs.RecepteurCommandeMVC;
+import ntro.client.mvc.controleurs.RecepteurMessageMVC;
 import ntro.debogage.DoitEtre;
 import ntro.debogage.J;
+import ntro.messages.FabriqueMessage;
+import demo.client.Main;
 import demo.client.commandes.choisir_qui_commence.ChoisirQuiCommence;
 import demo.client.commandes.choisir_qui_commence.ChoisirQuiCommenceRecue;
 import demo.client.commandes.choisir_taille_grille.ChoisirTailleGrille;
@@ -12,12 +15,21 @@ import demo.client.pages.parametres.afficheurs.AfficheurParametres;
 import demo.client.pages.parametres.modeles.Parametres;
 import demo.client.pages.parametres.modeles.ParametresLectureSeule;
 import demo.client.pages.parametres.vues.VueParametres;
+import demo.messages.transmettre_qui_commence.MsgTransmettreQuiCommence;
+import demo.messages.transmettre_qui_commence.MsgTransmettreQuiCommencePourEnvoi;
+import demo.messages.transmettre_qui_commence.MsgTransmettreQuiCommenceRecu;
+import demo.messages.transmettre_taille.MsgTransmettreTaille;
+import demo.messages.transmettre_taille.MsgTransmettreTaillePourEnvoi;
+import demo.messages.transmettre_taille.MsgTransmettreTailleRecu;
 
 public class   ControleurParametres 
        extends ControleurModeleVue<ParametresLectureSeule, 
                                    Parametres,
                                    VueParametres,
                                    AfficheurParametres> {
+	
+	private MsgTransmettreQuiCommencePourEnvoi msgTransmettreQuiCommence;
+	private MsgTransmettreTaillePourEnvoi msgTransmettreTaille;
 
 	@Override
 	protected void installerReceptionCommandes() {
@@ -32,6 +44,10 @@ public class   ControleurParametres
 				
 				getModele().choisirQuiCommence(commande.getCouleur());
 				
+				if(Main.siConnecteAuServeur()) {
+					msgTransmettreQuiCommence.setQuiCommence(commande.getCouleur());
+					msgTransmettreQuiCommence.envoyerMessage();
+				}
 			}
 		});
 		
@@ -43,7 +59,11 @@ public class   ControleurParametres
 				DoitEtre.nonNul(commande.getTailleGrille());
 				
 				getModele().choisirTailleGrille(commande.getTailleGrille());
-				
+
+				if(Main.siConnecteAuServeur()) {
+					msgTransmettreTaille.setTailleGrille(commande.getTailleGrille());
+					msgTransmettreTaille.envoyerMessage();
+				}
 			}
 		});
 	}
@@ -57,10 +77,32 @@ public class   ControleurParametres
 	@Override
 	protected void obtenirMessagesPourEnvoi() {
 		J.appel(this);
+		
+		msgTransmettreQuiCommence = FabriqueMessage.obtenirMessagePourEnvoi(MsgTransmettreQuiCommence.class);
+		msgTransmettreTaille = FabriqueMessage.obtenirMessagePourEnvoi(MsgTransmettreTaille.class);
 	}
 
 	@Override
 	protected void installerReceptionMessages() {
 		J.appel(this);
+		
+		installerRecepteurMessage(MsgTransmettreQuiCommence.class, new RecepteurMessageMVC<MsgTransmettreQuiCommenceRecu>() {
+
+			@Override
+			public void recevoirMessageMVC(MsgTransmettreQuiCommenceRecu messageRecu) {
+				J.appel(this);
+				
+				getModele().choisirQuiCommence(messageRecu.getQuiCommence());
+			}
+		});
+		
+		installerRecepteurMessage(MsgTransmettreTaille.class, new RecepteurMessageMVC<MsgTransmettreTailleRecu>() {
+			@Override
+			public void recevoirMessageMVC(MsgTransmettreTailleRecu messageRecu) {
+				J.appel(this);
+				
+				getModele().choisirTailleGrille(messageRecu.getTailleGrille());
+			}
+		});
 	}
 }
