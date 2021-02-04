5e0820d63120dabce06e0a4bd70398e25099ba3c
Mathieu Bergeron <mathieu.bergeron@cmontmorency.qc.ca>
2021-02-04 11:22:10 -0500

    correctif pour tut09 tut10

---
+++ tut10/src/main/java/tutoriel/enumerations/TailleGrille.java
@@ -0,0 +1,48 @@
+// Copyright (C) (2020) (Mathieu Bergeron) (mathieu.bergeron@cmontmorency.qc.ca)
+//
+// This file is part of tutoriels4f5
+//
+// tutoriels4f5 is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// tutoriels4f5 is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with aquiletour.  If not, see <https://www.gnu.org/licenses/>
+
+
+package tutoriel.enumerations;
+
+import static tutoriel.Constantes.*;
+
+import ntro.debogage.J;
+
+public enum TailleGrille {
+	
+	PETITE(HAUTEUR_GRILLE_PETITE, LARGEUR_GRILLE_PETITE), 
+	MOYENNE(HAUTEUR_GRILLE_MOYENNE, LARGEUR_GRILLE_MOYENNE), 
+	GRANDE(HAUTEUR_GRILLE_GRANDE, LARGEUR_GRILLE_GRANDE);
+
+	private int hauteur;
+	private int largeur;
+	
+	private TailleGrille(int hauteur, int largeur) {
+		J.appel(this);
+
+		this.hauteur = hauteur;
+		this.largeur = largeur;
+	}
+	
+	public int getHauteur(){
+		return hauteur;
+	}
+
+	public int getLargeur(){
+		return largeur;
+	}
+}
